232a6af29b28783ece833b8d38c65189ff649c40
Sergio <sergiomc11756@gmail.com>
2024-10-25 20:13:47 +0200

    cambio

--- tema5/ejercicio4.php
+++ tema5/ejercicio4.php
@@ -1,8 +1,7 @@
-<?php
+<?php 
 function letraFoM(){
     return rand(0, 1) ? 'F' : 'M'; //Segun si es 0 o 1 creamos F o M
 }
-
 $arrayAsociativo = ["F" => 0, "M" => 0];
 for ($i = 0; $i < 100; $i++) { //Creamos el array de 100 letras y añadimos las F y las M al array asociativo
     $array[$i] = letraFoM();
