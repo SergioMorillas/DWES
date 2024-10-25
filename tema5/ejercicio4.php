75da30597510c59c42d826bd24b4d364fafa9a13
Sergio <sergiomc11756@gmail.com>
2024-10-25 20:13:03 +0200

    cambio

--- tema5/ejercicio4.php
+++ tema5/ejercicio4.php
@@ -1,21 +1,11 @@
-<!-- EJERCICIO 4: Recorrido de arrays asociativos I
-Rellena un array de 100 elementos de manera aleatoria con valores M o F (por
-ejemplo ["M", "M", "F", "M", ...]). Una vez completado, vuelve a recorrerlo y
-calcula cuántos elementos hay de cada uno de los valores almacenando el
-resultado en un array asociativo ['M' => 44, 'F' => 66] (no utilices variables
-para contar las M o las F, es decir la cuenta guardarla directamente en el array
-asociativo). Finalmente, muestra el resultado por pantalla
-F = 70
-M = 77
--->
 <?php
 function letraFoM(){
-    return rand(0, 1) ? 'F' : 'M';
+    return rand(0, 1) ? 'F' : 'M'; //Segun si es 0 o 1 creamos F o M
 }
-    $arrayAsociativo = ["F" => 0, "M" => 0];
-for ($i = 0; $i < 100; $i++) {//Creamos el array de 100 letras y añadimos las F y las M al array asociativo
+
+$arrayAsociativo = ["F" => 0, "M" => 0];
+for ($i = 0; $i < 100; $i++) { //Creamos el array de 100 letras y añadimos las F y las M al array asociativo
     $array[$i] = letraFoM();
-    ($array[$i] === "F") ?$arrayAsociativo["F"]++ :$arrayAsociativo["M"]++;
+    ($array[$i] === "F") ? $arrayAsociativo["F"]++ : $arrayAsociativo["M"]++; // Si la letra que generamos es "F" añadimos 1 al array asociativo en su posicion y viceversa
 }
 var_dump($arrayAsociativo);
-?>
